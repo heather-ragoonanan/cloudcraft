a0c7c72bf5e356459ddceb52954aa7adf2b58e37
heather-ragoonanan <heather.ragoonanan@hotmail.com>
2026-02-24 11:18:15 +0000

    feat: frontend workflow and deployment

--- infrastructure/lib/stacks/service.ts
+++ infrastructure/lib/stacks/service.ts
@@ -2,11 +2,26 @@ import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 
 export class ServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const frontendS3 = new s3.Bucket(this, 'FrontendBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      publicReadAccess: false,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      autoDeleteObjects: false
+    });
+
+    new cdk.CfnOutput(this, 'FrontendBucketName', {
+      value: frontendS3.bucketName,
+    });
+
 
     // Simple Lambda function
     const helloFn = new lambda.Function(this, 'HelloFunction', {
